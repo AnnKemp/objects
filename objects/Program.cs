fe0e2a5cbba0abc1975bbda07366df1a551b8af0
AnnKemp <ann.kemp@scarlet.be>
2021-10-12 22:34:36 +0200

    Class + objects

--- objects/Program.cs
+++ objects/Program.cs
@@ -2,14 +2,15 @@
 
 namespace objects
 {
-    class Car
+    class Flower
     {
         string color = "red";
-
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            Console.WriteLine(myObj.color);
+            Flower myObj1 = new Flower();
+            Flower myObj2 = new Flower();
+            Console.WriteLine(myObj1.color);
+            Console.WriteLine(myObj2.color);
         }
     }
 }
