453a7a14d55fcd4eab6dd75c7b942790323e6135
AnnKemp <ann.kemp@scarlet.be>
2021-10-14 17:21:50 +0200

    a class in a class with multiple members and public methods

--- objects/Program.cs
+++ objects/Program.cs
@@ -8,6 +8,10 @@ namespace objects
         string model;
         string color;
         int year;
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
         // this class hasn't the word class in front of it
         static void Main(string[] args)
         {
@@ -15,6 +19,7 @@ namespace objects
             Ford.model = "Mustang";
             Ford.color = "red";
             Ford.year = 1969;
+            Ford.fullThrottle();
 
             Car Opel = new Car();
             Opel.model = "Astra";
@@ -23,6 +28,10 @@ namespace objects
 
             Console.WriteLine(Ford.model);
             Console.WriteLine(Opel.model);
+            /*
+             * Why did we declare the method as public, and not static, like in the examples from the C# Methods Chapter? 
+The reason is simple: a static method can be accessed without creating an object of the class, while public methods can only be accessed by objects.
+             */
         }
     }
 }
