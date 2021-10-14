57ccf54769f3e41a0c7805838e483910d70ad6ad
AnnKemp <ann.kemp@scarlet.be>
2021-10-14 16:56:07 +0200

    a class into a class

--- objects/Program.cs
+++ objects/Program.cs
@@ -2,28 +2,27 @@
 
 namespace objects
 {
-    //You can also create an object of a class and access it in another class. This is often used for better organization of classes (one class has all the fields and methods, while the other class holds the Main() method (code to be executed)).
+    // or create a class into another class ... seems without the word class at the start ...
     class Car
     {
-        //Did you notice the public keyword? It is called an access modifier, which specifies that the color variable/field of Car is accessible for other classes as well, such as Program.
-        // Class members
-        public string color = "red"; // field
-        public int maxSpeed;          // field
-        public void fullThrottle()   // method
-        {
-            Console.WriteLine("The car is going as fast as it can!");
-        }
-    }
-    class Program
-    {
+        string model;
+        string color;
+        int year;
+        // this class hasn't the word class in front of it
         static void Main(string[] args)
         {
-            Car myObj = new Car();
-            myObj.color = "blue";
-            myObj.maxSpeed = 200;
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(myObj.maxSpeed);
-            myObj.fullThrottle();
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 }
